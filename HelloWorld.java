d00a3a22c492aca41f95081380b5fa6e4a1b6b0d
Abhishek Dubey <abhishek.dubey.7190@gmail.com>
2026-03-23 22:36:46 +0530

    second commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld
+{
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Worlddddddd");
+		
+		try{
+			Thread.Sleep(50000);
+			}
+		catch(Exception e)
+		{		
+			e.printStackTrace();
+		}
+}
